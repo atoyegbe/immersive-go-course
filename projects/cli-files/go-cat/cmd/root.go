3866088a2719c075f94b950e7356a439e7b26999
Adeyemi Atoyegbe <herdeyemie@gmail.com>
2024-02-10 22:26:26 +0100

    support for -n command

--- projects/cli-files/go-cat/cmd/root.go
+++ projects/cli-files/go-cat/cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func Execute() {
@@ -10,18 +11,33 @@ func Execute() {
 		fmt.Println("Usage: <program> <file1> [<file2> ...]")
 		return
 	}
-	
+
 	commands := os.Args[1:]
+	var appendLine bool = false
 	for _, command := range commands {
+		if strings.HasPrefix(command, "-") {
+			if command == "-n" {
+				appendLine = true
+			} else {
+				fmt.Printf("Unknown option: %s\n", command)
+			}
+			continue
+		}
 		fileContent, err := os.ReadFile(command)
 		if err != nil {
 			fmt.Print(err)
 			continue
 		}
-
-		if _, err := os.Stdout.Write(fileContent); err != nil {
-			fmt.Printf("Error writing content of file %s to stdout: %v\n", command, err)
+		if appendLine {
+			lines := strings.Split(string(fileContent), "\n")
+			for i, line := range lines {
+				fmt.Printf("%6d  %s\n", i+1, line)
+			}
+		} else {
+			if _, err := os.Stdout.Write(fileContent); err != nil {
+				fmt.Printf("Error writing content of file %s to stdout: %v\n", command, err)
+			}
+			fmt.Println("")
 		}
-		fmt.Println("")
 	}
 }
